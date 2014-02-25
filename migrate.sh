cb4c4b3fa575c1149aa02eed0c026fcbe385ad75
Peter Withers <Peter.Withers@mpi.nl>
2014-02-25 14:04:03 +0100

    Preparing to clone the KinOath Kinship Archiver from the internal SVN to GitHub on the request of Daan Broeder <Daan.Broeder@mpi.nl>.

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,3 @@
 # This script exists only to migrate the svn branches correctly into git. 
 # Secondarily this script will also migrate the tickets from track to github, hopefully with all the commit triggers/references intact.
+git svn clone https://svn.mpi.nl/LAT --authors-file=users.txt --trunk=Kinnate/trunk --branches=Kinnate/branches --branches=documentation/manuals/kinoath
